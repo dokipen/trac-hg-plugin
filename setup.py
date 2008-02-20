5efe54cd730dad7d496d4840f2fcf390415cfd39
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2008-02-20 14:23:01 +0000

    TracMercurial for 0.11: remove old  method (#6822) and adapt requirement on Trac version.
    git-svn-id: http://svn.edgewall.org/repos/trac/sandbox/mercurial-plugin-0.11@6573 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 TracMercurial = 'http://trac.edgewall.org/wiki/TracMercurial',
 
 setup(name='TracMercurial',
+      install_requires='Trac >=0.11dev, ==0.11b1',
       description='Mercurial plugin for Trac 0.11',
       keywords='trac scm plugin mercurial hg',
       version='0.11.0.2',
@@ -13,7 +14,7 @@ setup(name='TracMercurial',
       author='Christian Boos',
       author_email='cboos@neuf.fr',
       long_description="""
-      This plug for Trac 0.11 provides support for the Mercurial SCM.
+      This plugin for Trac 0.11 provides support for the Mercurial SCM.
       
       See %s for more details.
       """ % TracMercurial,
