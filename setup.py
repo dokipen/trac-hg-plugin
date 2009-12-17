377cc2e66a64361ed294177c743ca3c5c5e9bd49
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2009-12-17 10:37:00 +0000

    TracMercurial for 0.12: versions 0.12.0.1x of the plugin will be for trunk before the MultiRepos merge.
    git-svn-id: http://svn.edgewall.org/repos/trac/plugins/0.12/mercurial-plugin@8947 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ from setuptools import setup, find_packages
 TracMercurial = 'http://trac.edgewall.org/wiki/TracMercurial'
 
 setup(name='TracMercurial',
-      install_requires=('Trac ==0.11, ==0.11rc2, ==0.11rc1, '
-                        ' ==0.11b2, ==0.11b1, >=0.11dev'),
-      description='Mercurial plugin for Trac 0.11',
+      install_requires=('Trac >=0.12dev'),
+      description='Mercurial plugin for Trac 0.12',
       keywords='trac scm plugin mercurial hg',
-      version='0.11.0.8',
+      version='0.12.0.10',
       url=TracMercurial,
       license='GPL',
       author='Christian Boos',
       author_email='cboos@neuf.fr',
       long_description="""
-      This plugin for Trac 0.11 provides support for the Mercurial SCM.
+      This plugin for Trac 0.12 (trunk) provides support for the Mercurial SCM.
       
       See %s for more details.
       """ % TracMercurial,
