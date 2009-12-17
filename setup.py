d6ea145c5a15d1bc7470bafeba87616720c69ea2
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2009-12-17 11:51:46 +0000

    TracMercurial for MultiRepos: versions 0.12.0.2x of the plugin will be for the MultiRepos variant of the plugin.
    git-svn-id: http://svn.edgewall.org/repos/trac/plugins/multirepos/mercurial-plugin@8950 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ except ImportError:
 TracMercurial = 'http://trac.edgewall.org/wiki/TracMercurial'
 
 setup(name='TracMercurial',
-      install_requires='Trac >=0.12dev',
-      description='Mercurial plugin for Trac 0.12',
+      install_requires='Trac >=0.12multirepos',
+      description='Mercurial plugin for Trac multirepos branch',
       keywords='trac scm plugin mercurial hg',
-      version='0.12.0.9',
+      version='0.12.0.20',
       url=TracMercurial,
       license='GPL',
       author='Christian Boos',
@@ -28,8 +28,7 @@ setup(name='TracMercurial',
       long_description="""
       This plugin for Trac 0.12 provides support for the Mercurial SCM.
 
-      '''Actually, to take full benefit of this version of the plugin,
-      the http://trac.edgewall.org/browser/sandbox/multirepos branch is
+      '''The http://trac.edgewall.org/browser/sandbox/multirepos branch is
       required.'''
       
       See %s for more details.
